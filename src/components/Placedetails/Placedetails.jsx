3c6e60898de35e23daa3a681c1b70c0b65e2e97f
aliekmandal <aliekmandal24@gmail.com>
2022-06-15 00:35:45 +0530

    added basic folder structure

---
+++ src/components/Placedetails/Placedetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Placedetails = () => {
+    return (
+        <div>
+            <h1>
+                Placedetails
+            </h1>
+        </div>
+    );
+}
+
+export default Placedetails;
